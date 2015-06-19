8475010d4e88a6e427de8076e466b30009ee7e53
Brendon J. Brewer <brendon.brewer@gmail.com>
2015-06-19 13:10:22 +1200

    Posterior plot

--- plots.py
+++ plots.py
@@ -7,15 +7,30 @@ rc("text", usetex=True)
 x = 1 + arange(0, 10)
 
 # Make a prior
-p = exp(0.5*randn(x.size))
+q = exp(0.5*randn(x.size))
+q /= q.sum()
+
+bar(x-0.5*0.35, q, width=0.35, alpha=0.3)
+xlabel("$x$", fontsize=20)
+ylabel("Probability, $q(x)$")
+xlim([0.5, 10.5])
+gca().set_xticks(x)
+title("Prior Distribution")
+savefig("distribution.pdf", bbox_inches="tight")
+show()
+
+
+# Make a posterior
+p = q.copy()
+p[logical_or(x < 3, x > 5)] = 0.
 p /= p.sum()
 
 bar(x-0.5*0.35, p, width=0.35, alpha=0.3)
 xlabel("$x$", fontsize=20)
-ylabel("Probability")
+ylabel("Probability, $p(x)$")
 xlim([0.5, 10.5])
 gca().set_xticks(x)
-title("A Probability Distribution")
-savefig("distribution.pdf", bbox_inches="tight")
+title("Posterior Distribution")
+savefig("distribution2.pdf", bbox_inches="tight")
 show()
 
